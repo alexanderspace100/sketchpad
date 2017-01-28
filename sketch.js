bc91a33e5b36df85a449bfebbfebbfb2a21c20eb
alexanderspace100 <alexanderspace100@gmail.com>
2017-01-28 21:52:25 +0200

    Adds the grid.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,23 @@
-$(document).ready(function(){
-  //builds the grid
-  var buildSketch = function(size){
-    var numRows =  size;
-    var numCols =  size;
+var gridSize = 64;
+var squareSize = 800/gridSize;
+var isBorder = false;
 
-    for (var i = 0; i < numRows; i++){
-      $(".container").append("<div class='row'></div>");
-    };
-})
+$(document).ready(function() {
+  startGrid();
+});
+
+function startGrid() {
+    // rows
+  for(i=0; i<gridSize*gridSize; i++) {
+    $(".container").append("<div class='grid-square'></div>")
+  }
+
+  // Adjust the Size
+  $(".grid-square").css("height", squareSize-2); // -2 to make up for the 1px border on each side.
+  $(".grid-square").css("width", squareSize-2);
+
+  // Set border/grid properly.
+  if(isBorder) {
+    $(".grid-square").css("border-color", "#4286f4");
+  }
+}
